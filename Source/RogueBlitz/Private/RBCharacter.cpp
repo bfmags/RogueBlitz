a1fc9e5b25bc5bd20e14ade0fb78ca0be23c51a5
bfmags <bfmags@gmail.com>
2024-02-25 00:02:35 +0000

    add PublicIncludePaths

--- Source/RogueBlitz/Private/RBCharacter.cpp
+++ Source/RogueBlitz/Private/RBCharacter.cpp
@@ -1,8 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "RBCharacter.h"
-
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "DrawDebugHelpers.h"
--- Source/RogueBlitz/RogueBlitz.Build.cs
+++ Source/RogueBlitz/RogueBlitz.Build.cs
@@ -7,14 +7,21 @@ public class RogueBlitz : ModuleRules
 	public RogueBlitz(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+
+		// Simplify the include paths in our source files
+		PublicIncludePaths.AddRange(
+			new string[] {
+				"RogueBlitz"
+			}
+		);
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
